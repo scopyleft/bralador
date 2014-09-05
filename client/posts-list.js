3c4c0bb417eca0e7d64e3c4f1d5d9cc860b9c099
Pntbr <pntbr@scopyleft.fr>
2014-09-05 22:34:15 +0200

    Remove posts navigation on pages

--- client/posts-list.js
+++ client/posts-list.js
@@ -7,15 +7,3 @@ Template.postsList.helpers({
     return (this.tagQuery);
   }
 });
-
-Template.postsList.events({
-  'click a': function () {
-    $('div#post').hide().fadeIn(300);
-  },
-  'mouseover': function () {
-    $('nav#posts-list').css({opacity: 1});
-  },
-  'mouseout': function () {
-    $('nav#posts-list').css({opacity: 0.7});
-  }
-});
