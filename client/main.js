3b11fd854b2a1745bde65d162fd8a933a686a079
Pntbr <pntbr@scopyleft.fr>
2014-09-09 17:11:03 +0200

    Change menu posts

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ Meteor.startup(function () {
   Meteor.subscribe("all-posts");
   Meteor.call('infos', '', function (err, infos) {
     Session.set("blogName", infos.blog.name);
+    Session.set("menuTitle", infos.blog.menu_title);
     document.title = infos.blog.title;
   });
 });
--- client/posts-list.js
+++ client/posts-list.js
@@ -1,4 +1,5 @@
 Template.postsList.helpers({
+  menuTitle: Session.get("menuTitle"),
   posts: function () {
     var query = (this.tagQuery) ? {tag: this.tagQuery} : {};
     return Posts.find(query, {fields: {content: 0}});
