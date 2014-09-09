0b0584ed884f7c5af7e764e86fb80a9f1e034888
Pntbr <pntbr@scopyleft.fr>
2014-09-09 16:25:33 +0200

    Remove contribution package

--- packages/contribution/package.js
+++
@@ -1,9 +0,0 @@
-Package.describe({
-  summary: "A module to share contents, articles, datas, posts",
-  version: "0.1.1",
-  git: "https://github.com/scopyleft/bralador-contribution"
-});
-
-Package.onUse(function (api, where) {
-  api.versionsFrom('METEOR@0.9.0.1');
-});
