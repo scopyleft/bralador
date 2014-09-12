f4f776b5073f6d4d278111a035ee74a3f1a94888
Pntbr <pntbr@scopyleft.fr>
2014-09-13 01:23:18 +0200

    Reduce line-height for titles

--- packages/blog/package.js
+++ packages/blog/package.js
@@ -5,15 +5,20 @@ Package.describe({
 
 Package.onUse(function (api, where) {
   api.versionsFrom('METEOR@0.9.1.1');
+
+  api.use(['templating'], 'client');
+
   api.add_files(['private/_infos.json'], ['server'], {isAsset: true});
   api.add_files(['lib/collections.js'], ['server', 'client']);
   api.add_files([
     'lib/server/posts.js',
     'lib/server/infos.js'
   ], ['server']);
-  
   api.add_files([
     'lib/client/routes.js',
+    'lib/client/post/post.html',
+    'lib/client/post/post.css',
+    'lib/client/post/post.js',
     'lib/client/main.js'
   ], ['client']);
 
