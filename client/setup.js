31a673a2da4e6b435d3e1b1bbf751720c6da0017
pointbar <stephane.langlois@scopyleft.fr>
2015-02-15 01:37:23 +0100

    Change repo

--- client/setup.js
+++ client/setup.js
@@ -1,5 +1,5 @@
 github_repo.list = [
-  {label: 'gymkhana', owner: 'pointbar', repo: 'gymkhana-contenu'}
+  {label: 'gymkhana', owner: 'gymkhana-fr', repo: 'gymkhana-contenu'}
 ];
 
 blog = {title: "GYMKHANA"};
